10bb2e046a02b2a4bbc3200882bea6dba6dc5ffe
Cpgreenig <96381060+Cpgreenig@users.noreply.github.com>
2025-06-23 15:51:32 +0100

    product schema

--- src/app/resources/page.tsx
+++ src/app/resources/page.tsx
@@ -2,7 +2,7 @@ import { Metadata } from 'next';
 import BlogPageClient from './BlogPageClient';
 
 export const metadata: Metadata = {
-  title: 'Resources | Tracer',
+  title: 'Tracer | Resources',
   description: 'Explore our latest blog posts, whitepapers, case studies, and helper tools to support bioinformatic workflows.',
 };
 
