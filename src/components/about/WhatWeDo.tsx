25637c04bc8504e6e21184efc14170e2d1758da8
Cpgreenig <96381060+Cpgreenig@users.noreply.github.com>
2025-07-07 12:57:17 +0100

    about us changes

--- src/components/about/WhatWeDo.tsx
+++ src/components/about/WhatWeDo.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function WhatWeDo() {
   return (
     <section className="relative overflow-hidden bg-[#1e1e1e] -mt-16 md:-mt-20 lg:-mt-24 xl:-mt-28 2xl:-mt-32">
-      <div className="relative z-20 pt-56 pb-20 md:pt-56 md:pb-48 lg:pt-64 lg:pb-56 xl:pt-72 xl:pb-64 2xl:pt-96 2xl:pb-72">
+      <div className="relative z-20 pt-56 pb-20 md:pt-56 md:pb-48 lg:pt-48 lg:pb-56 xl:pt-52 xl:pb-64 2xl:pt-72 2xl:pb-72">
         {/* Title and Description with alignment matching Why We Exist */}
         <div className="flex flex-col justify-center px-4 md:px-8 lg:px-12 lg:max-w-[1400px] lg:w-full">
           {/* Title */}
@@ -19,7 +19,7 @@ export default function WhatWeDo() {
       </div>
 
       {/* T-Asset-Edge Image - Right Edge */}
-      <div className="absolute right-0 top-0 md:top-0 2xl:top-64 z-30">
+      <div className="absolute right-0 top-0 md:top-0 xl:top-16 2xl:top-32 z-30">
         <div className="relative w-[200px] md:w-[300px] lg:w-[400px] xl:w-[500px] 2xl:w-[600px] h-[300px] md:h-[500px] lg:h-[600px] xl:h-[500px] 2xl:h-[450px]">
           <Image
             src="/images/about-us/T-Asset-Edge.webp"
